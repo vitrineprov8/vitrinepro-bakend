48edd814f182ca3281846da1bd066324f7cce136
AndresMHG <andr3sm1guel@gmail.com>
2026-02-19 18:53:36 -0300

    fix(cors): update CORS configuration to allow localhost:3001

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Habilitar CORS para localhost:4321
+  // Habilitar CORS para localhost:3001
   app.enableCors({
     origin: 'http://localhost:4321',
     credentials: true,
