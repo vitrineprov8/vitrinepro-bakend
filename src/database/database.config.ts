f1d3deb8b359e219ab358780c9a2e256266c87db
AndresMHG <andr3sm1guel@gmail.com>
2026-02-20 11:25:41 -0300

    refactor(database): update database configuration to use DATABASE_URL and SSL settings
    - Replaced individual database connection parameters with a single DATABASE_URL environment variable.
    - Added SSL configuration to enhance security for database connections.

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -3,11 +3,10 @@ import { User } from '../users/user.entity';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'vitronepro',
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [User],
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV !== 'production',
